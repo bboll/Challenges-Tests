5764d337f7dd2b7e22c0d4259f0c1e032461565c
Brian Boll <bboll@kent.edu>
2018-03-14 13:51:08 -0400

    Rename case_parser.cpp to case_parser2.cpp
    Lol

--- C++/Input Parsing/case_parser2.cpp
+++ C++/Input Parsing/case_parser2.cpp
@@ -30,11 +30,11 @@ int main()
   sscanf(CASE, "%d %d %d %d %d %d %d %d", x[0], y[0], x[1], y[1], x[2], y[2], x[3], y[3]);
 
   int idx=0;
-  bool shitflag=false;
+  bool checkForSolution=false;
   while(idx<4 && !shitflag)
   {
-    if(count(x, x+4, x[idx]) > 1){ cout << "no solution\n"; shitflag=true; }
-    if(count(y, y+4, y[idx]) > 1 && !shitflag){ cout << "no solution\n"; shitflag=true;}
+    if(count(x, x+4, x[idx]) > 1){ cout << "no solution\n"; checkForSolution=true; }
+    if(count(y, y+4, y[idx]) > 1 && !checkForSolution){ cout << "no solution\n"; checkForSolution=true;}
     ++idx;
   }
 
